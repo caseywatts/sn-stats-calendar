76b7792af2adc8b20ba1f3d9bc410141ef2434db
caseywatts <casey.s.watts@gmail.com>
2013-04-12 18:23:06 -0400

    works with missing resolve time

--- daybinning.rb
+++ daybinning.rb
@@ -5,8 +5,8 @@ require 'activesupport'
 CSV_FILE = "incident.csv"
 csvfile = CSV.read(CSV_FILE, :headers => true)
 csvfile.each do |inc|
-  inc["start_date"] = Date.strptime(inc["opened_at"], "%Y-%m-%d %H:%M:%S")
-  inc["end_date"] = Date.strptime(inc["u_resolved"], "%Y-%m-%d %H:%M:%S")
+  inc["start_date"] = Date.strptime(inc["opened_at"], "%Y-%m-%d %H:%M:%S") if inc["opened_at"]
+  inc["end_date"] = Date.strptime(inc["u_resolved"], "%Y-%m-%d %H:%M:%S") if inc["u_resolved"]
 end
 
 #For the range of dates in the csvfile, figure out how many tickets opened per day
@@ -28,10 +28,10 @@ CSV.open("incidentsresolved.csv", "w") do |csvoutput|
 end
 
 #For the range of dates in the csvfile, figure out how many tickets we had open each day
-CSV.open("incidentsactive.csv", "w") do |csvoutput|
-  csvoutput << ["date", "value"]
-  (csvfile["end_date"].min..csvfile["end_date"].max).each do |date|
-    count = csvfile["end_date"].count { |d| d == date}
-    csvoutput << [date.to_s, count.to_s]
-  end
-end
+#CSV.open("incidentsactive.csv", "w") do |csvoutput|
+  #csvoutput << ["date", "value"]
+  #(csvfile["end_date"].min..csvfile["end_date"].max).each do |date|
+    #count = csvfile["end_date"].count { |d| d == date}
+    #csvoutput << [date.to_s, count.to_s]
+  #end
+#end
