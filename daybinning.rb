950282c35df79442714a6aaa65d396f0cbefb7fc
caseywatts <casey.s.watts@gmail.com>
2013-04-12 19:10:33 -0400

    include incidents not yet resolved, for opened incidents

--- daybinning.rb
+++ daybinning.rb
@@ -5,8 +5,8 @@ require 'activesupport'
 CSV_FILE = "incident.csv"
 csvfile = CSV.read(CSV_FILE, :headers => true)
 csvfile.each do |inc|
-  inc["start_date"] = Date.strptime(inc["opened_at"], "%Y-%m-%d %H:%M:%S") if inc["opened_at"]
-  inc["end_date"] = Date.strptime(inc["u_resolved"], "%Y-%m-%d %H:%M:%S") if inc["u_resolved"]
+  inc["start_date"] = Date.strptime(inc["opened_at"], "%Y-%m-%d %H:%M:%S") unless inc["opened_at"].blank?
+  inc["end_date"] = Date.strptime(inc["u_resolved"], "%Y-%m-%d %H:%M:%S") unless inc["u_resolved"].blank?
 end
 
 #For the range of dates in the csvfile, figure out how many tickets opened per day
@@ -21,8 +21,10 @@ end
 #For the range of dates in the csvfile, figure out how many tickets resolved per day
 CSV.open("incidentsresolved.csv", "w") do |csvoutput|
   csvoutput << ["date", "value"]
-  (csvfile["end_date"].min..csvfile["end_date"].max).each do |date|
-    count = csvfile["end_date"].count { |d| d == date}
+  mindate = csvfile["end_date"].each.reject{|x| x.nil?}.min
+  maxdate = csvfile["end_date"].each.reject{|x| x.nil?}.max
+  (mindate..maxdate).each do |date|
+    count = csvfile["end_date"].count { |d| d == date }
     csvoutput << [date.to_s, count.to_s]
   end
 end
